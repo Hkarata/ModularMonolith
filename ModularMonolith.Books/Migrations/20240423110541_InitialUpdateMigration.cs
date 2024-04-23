1a89153d44db87ce051c1d7cdd87d106b15b7d1d
hei karata <herishine00@gmail.com>
2024-04-23 15:23:49 +0300

    Add InitialUpdateMigration for Books module
    This commit introduces the `InitialUpdateMigration` for the books and authors module in a .NET Core project using Entity Framework Core. The migration sets up the initial database schema for a SQL Server database. The `InitialUpdateMigration.Designer.cs` file defines the `BooksDbContext` with three entities: `AuthorBook`, `Author`, and `Book`. `AuthorBook` is a join table for a many-to-many relationship between `Author` and `Book`. The `Author` and `Book` entities have common properties and specific ones like `Name` for `Author` and `Title` and `DatePublished` for `Book`. The `InitialUpdateMigration.cs` file contains the `Up` and `Down` methods for applying and reverting the migration respectively, creating and dropping unique indexes on the `Title` and `Name` columns.

---
+++ ModularMonolith.Books/Migrations/20240423110541_InitialUpdateMigration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModularMonolith.Books.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUpdateMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Title",
+                schema: "Books",
+                table: "Books",
+                column: "Title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_Name",
+                schema: "Books",
+                table: "Authors",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Books_Title",
+                schema: "Books",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Authors_Name",
+                schema: "Books",
+                table: "Authors");
+        }
+    }
+}
